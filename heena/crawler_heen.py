297190b2416bf04f3effa54f5e2c769991c4f007
LimHeena <dlagmlsk7@naver.com>
2023-04-17 18:35:37 +0900

    crawling file

--- heena/crawler_heen.py
+++ heena/crawler_heen.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 from cleansing import cleansing
+from datetime import datetime, timedelta
 
 #----------------------------------------------------------
 class Crawler:
@@ -19,7 +20,7 @@ class Crawler:
         # 마지막 뉴스 url dict 가져오기
         with open('./naver_news_url', 'r') as f:
             tmp = [l.rstrip().split(',') for l in open('./naver_news_url', encoding='utf-8').readlines()]
-            self.SUBCATEGORY_URL = {int(k): v for k, v in tmp}
+            self.SUB_CATEGORY_URL = {int(k): v for k, v in tmp}
 
     def naver_crawler(self):
     
@@ -37,134 +38,166 @@ class Crawler:
         for sub_num in sub_category_num:
             
             URL = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2={}&sid1=105".format(sub_num) #IT/과학
-            print('서브카테고리 하나 크롤링 중~')
-
-            #총 페이지 수 구하기
-            URL = URL + "&page={}"
-            page = 1
-
-            while True:
-                response = requests.get(URL.format(page), headers=headers)    
-                soup = BeautifulSoup(response.text, 'html.parser')
-                try: 
-                    if soup.select_one('.next').text == '다음':
-                        page += 1
-                except:
-                    break
-                
-            #date는 while문 돌려서 최근기사부터 시작. last url 저장되어있는 날짜에서 중지.
-            global breaker_1 
-            breaker_1 = False
-            global breaker_2 
-            breaker_2 = False
+            print(f'서브카테고리({sub_num}) 하나 크롤링 중~')
             
-            while True:
-                if breaker_2 == True:
+            date = datetime.now()
+
+            global breaker_3
+            breaker_3 = False
+            
+            while True: #date 주기
+                if breaker_3 == True:
                     break
                 
-                if page > 2:
-                    for_pages = soup.find(class_="paging" )
-                    try:
-                        pages = for_pages.find_all('a')
-                        plus_page = len(pages) - 1
-                    except: pass
-                else:
-                    try:
-                        for_pages = soup.find(class_="paging" )
-                        pages = for_pages.find_all('a')
-                        plus_page = len(pages)
-                    except:
-                        plus_page = 1
-
-                total_page = page + plus_page
-
-                for page in range(1,total_page+1):
-                    
-                    if breaker_1 == True:
-                        breaker_2 = True
-                        break
-
-                    response = requests.get(URL.format(page), headers=headers)    
+                date_str = datetime.strftime(date, '%Y%m%d')
+                print(f'{date_str} 크롤링 중')
+                
+                first_URL = URL + f'&date={date_str}'
+                use_URL = URL + '&page={}'
+                #print(first_URL)
+                
+                page = 1
+                
+                #총 페이지 수 구하기
+                while True:
+                    response = requests.get(first_URL, headers=headers)    
                     soup = BeautifulSoup(response.text, 'html.parser')
-
-                    url_in = soup.select('div[id="main_content"] li') #한 페이지 최대 20개
-                    url_list = []
-                    for li in url_in:
-                        url_one = li.select_one('dt a[href]').attrs['href'] #기자와 작성시간 알기 위해 기사 링크 추가
-                        url_list.append(url_one)
-
-                    for url in url_list: #한 링크씩 들어가기
-
-                        response = requests.get(url, headers=headers)
-                        time.sleep(1.5)
-                        soup = BeautifulSoup(response.text, 'html.parser')
-                        
-                        self.news_url = url
-                        if self.news_url == self.SUBCATEGORY_URL[sub_num]: #최신뉴스까지만!
-                            print(f'[{SUB_CATEGORY_DICT[sub_num]}] 최신까지 크롤링 완료')
-                            breaker_1 = True
-                            break
-                            
-                        news_dict = {}
-                        news_dict['platform'] = '네이버'
-                        news_dict['main_category'] = 'IT/과학'
-                        
-                        sub_name = [l.rstrip().split(',') for l in open('./sub_category').readlines()]
-                        SUB_CATEGORY_DICT = {int(k): v for k, v in sub_name}
-                        
-                        sub_name = SUB_CATEGORY_DICT[sub_num]
-                        news_dict['sub_category'] = '{}'.format(sub_name)
-
+                    try: 
+                        if soup.select_one('.next').text == '다음':
+                            page += 1
+                    except:
+                        break
+                    
+                global breaker_1 
+                breaker_1 = False
+                global breaker_2 
+                breaker_2 = False
+                
+                global page_breaker
+                page_breaker = False
+                
+                while True:
+                    if breaker_2 == True:
+                        breaker_3 = True
+                        break
+                    
+                    if page_breaker == True:
+                        break
+                    
+                    if page > 2:
+                        for_pages = soup.find(class_="paging" )
                         try:
-                            news_dict['title'] = soup.select_one('.media_end_head_headline').text.strip()
+                            pages = for_pages.find_all('a')
+                            plus_page = len(pages) - 1
+                        except: pass
+                    else:
+                        try:
+                            for_pages = soup.find(class_="paging" )
+                            pages = for_pages.find_all('a')
+                            plus_page = len(pages)
                         except:
+                            plus_page = 1
+
+                    total_page = page + plus_page
+                    print(f'총 페이지는? {total_page} !!!!! ')
+
+                    for page in range(1,total_page+1):
+                        
+                        if breaker_1 == True:
+                            breaker_2 = True
+                            break
+
+                        response = requests.get(use_URL.format(page), headers=headers)    
+                        soup = BeautifulSoup(response.text, 'html.parser')
+
+                        url_in = soup.select('div[id="main_content"] li') #한 페이지 최대 20개
+                        url_list = []
+                        for li in url_in:
+                            url_one = li.select_one('dt a[href]').attrs['href'] #기자와 작성시간 알기 위해 기사 링크 추가
+                            url_list.append(url_one)
+                        
+                        #print(f'{page}페이지 링크 다 가져옴!!') 
+
+                        for url in url_list: #한 링크씩 들어가기
+
+                            response = requests.get(url, headers=headers)
+                            time.sleep(1.5)
+                            soup = BeautifulSoup(response.text, 'html.parser')
+                            
+                            self.news_url = url
+                            if self.news_url == self.SUB_CATEGORY_URL[sub_num]: #최신뉴스까지만!
+                                print(f'[{self.SUB_CATEGORY_DICT[sub_num]}] 최신까지 크롤링 완료')
+                                breaker_1 = True
+                                break
+                                
+                            news_dict = {}
+                            news_dict['platform'] = '네이버'
+                            news_dict['main_category'] = 'IT/과학'
+                            
+                            sub_names = [l.rstrip().split(',') for l in open('./sub_category').readlines()]
+                            self.SUB_CATEGORY_DICT = {int(k): v for k, v in sub_names}
+                            
+                            sub_name = self.SUB_CATEGORY_DICT[sub_num]
+                            news_dict['sub_category'] = '{}'.format(sub_name)
+
                             try:
                                 news_dict['title'] = soup.select_one('.media_end_head_headline').text.strip()
                             except:
                                 try:
-                                    news_dict['title'] = soup.select_one('h2[id="title_area"]').text.strip()
+                                    news_dict['title'] = soup.select_one('.media_end_head_headline').text.strip()
                                 except:
-                                    news_dict['title'] = soup.select_one('.title').text.strip()
-                        try:
-                            news_dict['content'] = soup.select_one('div[id="newsct_article"]').text.strip()
-                        except:
+                                    try:
+                                        news_dict['title'] = soup.select_one('h2[id="title_area"]').text.strip()
+                                    except:
+                                        news_dict['title'] = soup.select_one('.title').text.strip()
                             try:
-                                news_dict['content'] = soup.select_one('div[id="dic_area"]').text.strip()
+                                news_dict['content'] = soup.select_one('div[id="newsct_article"]').text.strip()
                             except:
                                 try:
-                                    news_dict['content'] = soup.select_one('div[id="articeBody"]').text.strip()
+                                    news_dict['content'] = soup.select_one('div[id="dic_area"]').text.strip()
                                 except:
-                                    news_dict['content'] = soup.select_one('.news_end').text.strip()
-                        try:
-                            news_dict['writer'] = soup.select_one('.media_end_head_journalist_name').text.strip()
-                        except:
+                                    try:
+                                        news_dict['content'] = soup.select_one('div[id="articeBody"]').text.strip()
+                                    except:
+                                        news_dict['content'] = soup.select_one('.news_end').text.strip()
                             try:
-                                news_dict['writer'] = soup.select_one('.byline_s').text[:6]
+                                news_dict['writer'] = soup.select_one('.media_end_head_journalist_name').text.strip()
                             except:
                                 try:
-                                    news_dict['writer'] = soup.select_one('.byline').text[:3].strip()
+                                    news_dict['writer'] = soup.select_one('.byline_s').text[:6]
                                 except:
-                                    news_dict['writer'] = None
-                        try:
-                            news_dict['writed_at'] = soup.select_one('.media_end_head_info_datestamp_bunch').text.strip()
-                        except:
+                                    try:
+                                        news_dict['writer'] = soup.select_one('.byline').text[:3].strip()
+                                    except:
+                                        news_dict['writer'] = None
                             try:
-                                news_dict['writed_at'] = soup.select_one('.author em').text.strip()
+                                news_dict['writed_at'] = soup.select_one('.media_end_head_info_datestamp_bunch').text.strip()
                             except:
-                                news_dict['writed_at'] = soup.select_one('.info').text.strip()
+                                try:
+                                    news_dict['writed_at'] = soup.select_one('.author em').text.strip()
+                                except:
+                                    news_dict['writed_at'] = soup.select_one('.info').text.strip()
 
-                        df_one = pd.DataFrame(news_dict, index = [0])
-                        news_list.append(df_one)
+                            df_one = pd.DataFrame(news_dict, index = [0])
+                            news_list.append(df_one)
+                            
+                            #print('한 기사 넣기 완료')
+                        print(f'{page} 완료!')    
+                        
+                        if page == total_page:
+                            page_breaker = True
+                        
+                date = date - timedelta(days=1)
             
             df = pd.concat(news_list, ignore_index=True)
                 
             df.drop_duplicates(['title','content']) #혹시 모를 중복 제거
             df.sort_values('writed_at')
             
-            self.SUBCATEGORY_URL[sub_num] = self.news_url
+            self.SUB_CATEGORY_URL[sub_num] = self.news_url
             
             with open('./naver_news_url', 'w', encoding='utf-8') as f: #마지막 url 저장
-                for sub_num1, sub_name1 in self.SUBCATEGORY_URL.items():
+                for sub_num1, sub_name1 in self.SUB_CATEGORY_URL.items():
                     f.write(f'{sub_num1},{sub_name1}\n')
         
         # crontab에서 10분단위 스케줄링, 카테고리 2개의 경우 소요시간 0초
